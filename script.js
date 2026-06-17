4310e5c050ade04f9a814f3ccf6725d307283c40
Manoj <digitalcreater.jkf@gmail.com>
2026-06-17 09:29:00 +0530

    JS

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("contactForm")
+.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    const response = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+    document.getElementById("status").innerText =
+        result.message;
+});
